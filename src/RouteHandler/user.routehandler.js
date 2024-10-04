762870ec5e07b2e911a01e94f3ff900ff23df11b
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-10-04 20:51:40 +0530

    test2

--- src/RouteHandler/user.routehandler.js
+++ src/RouteHandler/user.routehandler.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authRoutes = require("../Routes/Auth.route");
+const authRoutes = require("../Routes/auth.route");
 
 const meetRoutes= require("../Routes/meet.route")
 module.exports = (app) => {
